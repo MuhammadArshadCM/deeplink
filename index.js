be564b936c7f74de410b7f1d1f10c8cff63aecc1
MuhammadArshadCM <arshad.cm@bosctechlabs.com>
2024-05-17 14:11:45 +0530

    feat:dfad

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ app.get("/.well-known/apple-app-site-association", (req, res) => {
     applinks: {
       apps: [],
       details: [{
-        appID: "2L58PD84S5.in.bslearning.ximek",
-        paths: ["/deep-link/ximek*"],
+        appID: "2L58PD84S5.in.bslearning.businessstandard",
+        paths: ["/deep-link/businessstandard*"],
       }],
     },
   };
